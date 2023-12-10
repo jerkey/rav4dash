76e5c167c6b3a5e269d85573f9b12b9d17842d80
Demo User <jerkey@g-cipher.net>
2023-12-10 01:52:03 +0000

    SOMETIMES this gets the correct response from EngineCSvim rav4dash.py !

--- rav4dash.py
+++ rav4dash.py
@@ -3,8 +3,11 @@
 import serial
 from time import sleep
 
-init_rav4evmon3 = '81d5f181c8'
-init_rav4evmon3_response = '83f1d5c1e98f82'
+init_BCS_mode = '81d5f181c8'
+init_BCS_mode_response = '83f1d5c1e98f82'
+init_EngineCS_mode = '8116f18109'
+init_EngineCS_mode_response = '83f116c1e98fc3'
+init_EngineCS_DTCquery = '8116f11398'
 
 s = serial.Serial(port='/dev/ttyS4',baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
 
@@ -22,18 +25,45 @@ s.break_condition = False
 s.write(bytearray.fromhex('00'))
 sleep(0.01)
 #writehex('8116F18109') # got response ff03150016 with 0.01 delay
-#writehex(init_rav4evmon3)
+#writehex(init_BCS_mode)
 #writehex('daedbeeff00d')
 #s.write(bytearray.fromhex('8116F18109'))
-s.write(bytearray.fromhex(init_rav4evmon3))
+#s.write(bytearray.fromhex(init_BCS_mode))
+#sleep(0.5)
+#readback = s.read_all().hex()
+#print('readback: '+ readback)
+#whatwesent = readback.find(init_BCS_mode)
+#response = readback[whatwesent+len(init_BCS_mode):]
+#print('thats '+readback[:whatwesent]+'and then whatwesent and then '+response)
+#if response == init_BCS_mode_response:
+#    print('correct response to BCS init')
+#if response == init_EngineCS_mode_response:
+#    print('correct response to EngineCS init')
+#
+#sleep(1)
+
+s.write(bytearray.fromhex(init_EngineCS_mode))
+sleep(1)
+readback = s.read_all().hex()
+print('readback: '+ readback)
+whatwesent = readback.find(init_EngineCS_mode)
+response = readback[whatwesent+len(init_EngineCS_mode):]
+print('thats '+readback[:whatwesent]+'and then whatwesent and then '+response)
+if response == init_BCS_mode_response:
+    print('correct response to BCS init')
+if response == init_EngineCS_mode_response:
+    print('correct response to EngineCS init')
+
+sleep(1)
+
+s.write(bytearray.fromhex(init_EngineCS_DTCquery))
 sleep(0.5)
 readback = s.read_all().hex()
 print('readback: '+ readback)
-whatwesent = readback.find(init_rav4evmon3)
-response = readback[whatwesent+len(init_rav4evmon3):]
-print('thats '+readback[:whatwesent]+'and then init_rav4evmon3 and then '+response)
-if response == init_rav4evmon3_response:
-    print('correct response to init')
+whatwesent = readback.find(init_EngineCS_DTCquery)
+response = readback[whatwesent+len(init_EngineCS_DTCquery):]
+print('thats '+readback[:whatwesent]+'and then whatwesent and then '+response)
+
 
 
 exit()
