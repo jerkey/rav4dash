6dc2ac13c0ada363e3da93ffa1d798cbb8712025
Demo User <debian@bbbox.localdomain>
2023-12-10 01:35:57 +0000

    parse response bytes

--- rav4dash.py
+++ rav4dash.py
@@ -3,7 +3,8 @@
 import serial
 from time import sleep
 
-init_rav4evmon3 = '81D5F181C8'
+init_rav4evmon3 = '81d5f181c8'
+init_rav4evmon3_response = '83f1d5c1e98f82'
 
 s = serial.Serial(port='/dev/ttyS4',baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
 
@@ -26,13 +27,13 @@ sleep(0.01)
 #s.write(bytearray.fromhex('8116F18109'))
 s.write(bytearray.fromhex(init_rav4evmon3))
 sleep(0.5)
-print('response: '+ s.read_all().hex())
-sleep(1)
-print('response: '+ s.read_all().hex())
-
-
-
-
+readback = s.read_all().hex()
+print('readback: '+ readback)
+whatwesent = readback.find(init_rav4evmon3)
+response = readback[whatwesent+len(init_rav4evmon3):]
+print('thats '+readback[:whatwesent]+'and then init_rav4evmon3 and then '+response)
+if response == init_rav4evmon3_response:
+    print('correct response to init')
 
 
 exit()
