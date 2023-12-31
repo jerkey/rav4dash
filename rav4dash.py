fce9b8a3f25a677dcf9c07bcaac305e3fb60fb3e
Demo User <jerkey@g-cipher.net>
2023-12-31 09:13:27 +0000

    reading battery voltage and current once per second

--- rav4dash.py
+++ rav4dash.py
@@ -4,7 +4,7 @@ import serial
 from time import sleep
 
 # RTS will go True upon opening serial port, and False when program closes
-s = serial.Serial(port='/dev/ttyS4',baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=200, xonxoff=0, rtscts=0)
+s = serial.Serial(port='/dev/ttyS4',baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=2000, xonxoff=0, rtscts=0)
 
 BCS = 0xD5 # battery controller
 ECS = 0x16 # engine controller
@@ -17,20 +17,33 @@ def sendPacket(destination, data):
     toSend.append(checksum % 256)
     s.write(bytearray(toSend))
     readback = s.read(len(toSend)) # s.write() returns number of bytes sent
-    if readback == bytearray(toSend):
-        print("sent "+bytearray(toSend).hex())
-    else:
+    if readback != bytearray(toSend):
         print("sent "+bytearray(toSend).hex()+" but echo was "+readback.hex())
 
-def parseReply():
+def parseReply(printout=True):
     a = s.read_all()
     while len(a) == 0:
         print('.',end='')
+        sleep(0.01)
         a = s.read_all()
 
     if a[0] > 0x87 or a[0] < 0x81:
         print("first byte returned was "+hex(a[0])+" expected 0x81-0x87")
-    print("returned "+a.hex())
+    if a[0] & 15 != len(a) - 4:
+        print("expected "+str((a[0] & 15) + 4)+" bytes but got "+str(len(a)))
+    if printout:
+        if a[2] == ECS:
+            print("ECS says: ",end='')
+        if a[2] == BCS:
+            print("BCS says: ",end='')
+        print(a[3:(3 + a[0] & 15)].hex())
+    checksum = 0
+    for i in a[0:(3 + a[0] & 15)]:
+        checksum += i;
+    checksum %= 256
+    if checksum != a[len(a)-1]:
+        print("checksum is wrong, was "+str(a[len(a)-1])+" but expected "+str(checksum))
+    return a
 
 def initECS():
     s.write(bytearray.fromhex('00'))
@@ -69,6 +82,15 @@ def writehex(hexbytes):
         sleep(0.01)
 
 initBCS()
+while(1):
+    sendPacket(BCS,[0x21,0])
+    parseReply(printout=False)
+    sendPacket(BCS,[0x21,1])
+    volts = parseReply(printout=False)
+    sendPacket(BCS,[0x21,3])
+    amps = parseReply(printout=False)
+    print("Volts: "+str((volts[5]*256+volts[6])/10)+"	Amps: "+str(((amps[5]*256+amps[6])-65536)/10))
+    sleep(1)
 
 #s.setRTS(False) # True is +5.15v, False is -5.15v
 exit() # RTS will go to False upon exit
