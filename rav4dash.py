db698ac549480e969c322809de27e2ac2fbc3bec
Demo User <jerkey@g-cipher.net>
2024-01-02 03:38:45 +0000

    ACTUALLY correct signed integer conversion

--- rav4dash.py
+++ rav4dash.py
@@ -67,7 +67,7 @@ def initBCS():
     s.break_condition = False
     s.write(bytearray.fromhex('00'))
     sleep(0.01)
-    s.read_all() # throw away whatever is in the buffer
+    print("all: "+str(s.read_all())) # throw away whatever is in the buffer
     sendPacket(BCS,[0x81])
     parseReply()
     sendPacket(BCS,[0x12,0,0])
@@ -88,10 +88,10 @@ while(1):
     parseReply(printout=False)
     sendPacket(BCS,[0x21,1])
     v = parseReply(printout=False)
-    volts = (v[5]*256+v[6])/10
+    volts = int.from_bytes(v[5:7], byteorder='big', signed=True)/10.0   # (v[5]*256+v[6])/10
     sendPacket(BCS,[0x21,3])
     a = parseReply(printout=False)
-    amps = ((a[5]*256+a[6])-65535)/10
+    amps = int.from_bytes(a[5:7], byteorder='big', signed=True)/10.0   # ((a[5]*256+a[6])-65535)/10
     watts = volts * amps
     print("Volts: "+str(volts)+"	Amps: "+str(amps)+"	Watts: "+str(int(watts)))
     sleep(1)
