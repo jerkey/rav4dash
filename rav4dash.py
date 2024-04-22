6ebfea92476e15e3be3421a6a83e36b79ce34967
Demo User <jerkey@g-cipher.net>
2024-04-21 18:08:34 -0700

    add State Of Charge request

--- rav4dash.py
+++ rav4dash.py
@@ -5,7 +5,7 @@ import time
 import os
 
 # RTS will go True upon opening serial port, and False when program closes
-s = serial.Serial(port='/dev/ttyS4',baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=2000, xonxoff=0, rtscts=0)
+serialPort = serial.Serial(port='/dev/ttyS4',baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=2000, xonxoff=0, rtscts=0)
 
 BCS = 0xD5 # battery controller
 ECS = 0x16 # engine controller
@@ -18,20 +18,20 @@ def sendPacket(destination, data):
     for i in toSend:
         checksum += i;
     toSend.append(checksum % 256)
-    s.write(bytearray(toSend))
-    readback = s.read(len(toSend)) # s.write() returns number of bytes sent
+    serialPort.write(bytearray(toSend))
+    readback = serialPort.read(len(toSend)) # serialPort.write() returns number of bytes sent
     if readback != bytearray(toSend):
         print("sent "+bytearray(toSend).hex()+" but echo was "+readback.hex())
 
 def parseReply(printout=True):
-    a = s.read_all()
+    a = serialPort.read_all()
     startParseTime = time.time()
     while len(a) == 0 and (time.time() - startParseTime) < 5: # timeout in seconds
         print('.',end='')
         time.sleep(0.1)
-        a = s.read_all()
+        a = serialPort.read_all()
     if len(a) == 0:
-        s.read_all() # clear buffer
+        serialPort.read_all() # clear buffer
         return False
     if a[0] > 0x87 or a[0] < 0x81:
         print("first byte returned was "+hex(a[0])+" expected 0x81-0x87")
@@ -39,7 +39,7 @@ def parseReply(printout=True):
     if a[0] & 15 != len(a) - 4:
         print("strange, expected "+str((a[0] & 15) + 4)+" bytes but got "+str(len(a))+", reading for a while and printing all:",end='')
         time.sleep(1)
-        a += s.read_all()
+        a += serialPort.read_all()
         print(a.hex())
         return a
     checksum = 0
@@ -58,13 +58,13 @@ def parseReply(printout=True):
     return a
 
 def initECS():
-    s.write(bytearray.fromhex('00'))
-    s.break_condition = True # https://forums.raspberrypi.com/viewtopic.php?t=239406
+    serialPort.write(bytearray.fromhex('00'))
+    serialPort.break_condition = True # https://forums.raspberrypi.com/viewtopic.php?t=239406
     time.sleep(0.035)
-    s.break_condition = False
-    s.write(bytearray.fromhex('00'))
+    serialPort.break_condition = False
+    serialPort.write(bytearray.fromhex('00'))
     time.sleep(0.01)
-    s.read_all() # throw away whatever is in the buffer
+    serialPort.read_all() # throw away whatever is in the buffer
     sendPacket(ECS,[0x81])
     parseReply()
     sendPacket(ECS,[0x12,0x1F,0])
@@ -75,13 +75,13 @@ def initECS():
 def initBCS():
     initBCSStatus = False
     while initBCSStatus == False:
-        s.write(bytearray.fromhex('00'))
-        s.break_condition = True # https://forums.raspberrypi.com/viewtopic.php?t=239406
+        serialPort.write(bytearray.fromhex('00'))
+        serialPort.break_condition = True # https://forums.raspberrypi.com/viewtopic.php?t=239406
         time.sleep(0.035)
-        s.break_condition = False
-        s.write(bytearray.fromhex('00'))
+        serialPort.break_condition = False
+        serialPort.write(bytearray.fromhex('00'))
         time.sleep(0.01)
-        print("all: "+str(s.read_all())) # throw away whatever is in the buffer
+        print("all: "+str(serialPort.read_all())) # throw away whatever is in the buffer
         sendPacket(BCS,[0x81])
         initBCSStatus = parseReply()
     sendPacket(BCS,[0x12,0,0])
@@ -93,7 +93,7 @@ def writehex(hexbytes):
     print('writing ',end='')
     for i in bytearray.fromhex(hexbytes):
         print(hex(i),end=' ')
-        s.write(i)
+        serialPort.write(i)
         time.sleep(0.01)
 
 def getModuleVoltages():
@@ -123,14 +123,17 @@ while(failedParseReplies < 5):
     v = parseReply(printout=False)
     sendPacket(BCS,[0x21,3]) # request amperage
     a = parseReply(printout=False)
+    sendPacket(BCS,[0x21,4]) # request state of charge
+    s = parseReply(printout=False)
     if v and a:
         volts = int.from_bytes(v[5:7], byteorder='big', signed=True)/10.0   # (v[5]*256+v[6])/10
         amps = int.from_bytes(a[5:7], byteorder='big', signed=True)/10.0   # ((a[5]*256+a[6])-65535)/10
+        soc = int.from_bytes(s[5:7], byteorder='big', signed=True)/10.0   #
         watts = volts * amps
         if volts != 499.5 and amps != 400:
             totalEnergy += watts * ( time.time() - loopTime ) # add energy from each round
         loopTime = time.time() # update timer
-        printString = "Volts: "+str(volts)+"	Amps: "+str(amps)+"	Watts: "+str(int(watts))+"	Wh: "+str(int(totalEnergy/3600))
+        printString = "Volts: "+str(volts)+"	Amps: "+str(amps)+"	Watts: "+str(int(watts))+"	Wh: "+str(int(totalEnergy/3600)) + "	SOC: " + str(soc)
         print(printString)
         if (time.time() - webUpdateTime) > 60: # timeout in seconds
             os.system('curl -G https://website.org/cgi-bin/darbo --data-urlencode "' + printString + '"' )
@@ -142,5 +145,5 @@ while(failedParseReplies < 5):
         print("timed out querying for volts or amps, failedParseReplies = "+str(failedParseReplies))
     time.sleep(1)
 
-#s.setRTS(False) # True is +5.15v, False is -5.15v
+#serialPort.setRTS(False) # True is +5.15v, False is -5.15v
 exit() # RTS will go to False upon exit
